09a50d73941307917a703da17902be03f5fad8f7
Fabian Jevon <Shar_evonz@hotmail.com>
2024-12-05 05:35:07 +0700

    add improvement

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -28,25 +28,25 @@
                             <a href="#" class="block py-2 px-3" aria-current="page">Home</a>
                         </li>
                         <li class="border-b-4 hover:border-orange-600 border-transparent hover:text-orange-400">
-                            <a href="#" class="block py-2 px-3">Our Tours</a>
+                            <a href="#ourtour" class="block py-2 px-3">Our Tours</a>
                         </li>
                         <li class="border-b-4 hover:border-orange-600 border-transparent hover:text-orange-400">
-                            <a href="#" class="block py-2 px-3">Review</a>
+                            <a href="#review" class="block py-2 px-3">Review</a>
                         </li>
                         <li class="border-2 border-black rounded-full hover:text-orange-400 px-2">
-                            <a href="#" class="block py-2 px-3">Contact us</a>
+                            <a href="#contactus" class="block py-2 px-3">Contact us</a>
                         </li>
                     </ul>
                     </div>
                 </div>
             </nav>
 
-            <section class="bg-cover bg-center bg-no-repeat " style="background-image: url({{ $jumbotron_img }})">
+            <section class="bg-cover bg-center bg-no-repeat" style="background-image: url({{ $jumbotron_img }})">
                 <div class="px-4 ml-8 max-w-screen-xl text-center py-10 lg:pt-56 ">
                     <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl text-left">
                         {{$jumbotron_caption}}
                     </p>
-                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-left">
+                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-left grid grid-cols-3">
                         {{$jumbotron_headline}}
                     </h1>                    
                 </div>
@@ -140,11 +140,88 @@
                     <p>With an easy and fast ticket purchase process</p>
                 </div>
             </div>
+
+            <div id="ourtour" class="py-20">
+                <h2 class="text-3xl mb-10">Our Tour</h2>
+                <div class="grid grid-cols-2">
+                    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 inline-block">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/eiffeltwr.jpg" alt="">
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Paris, France</h5>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here is the eiffel tower.</p>
+                        </div>
+                    </a>
+
+                    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 inline-block">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/timessquare.jpg" alt="">
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Manhattan, United States</h5>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here is the Times Square.</p>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+
+            <div id="review" class="py-6 mt-10">
+            <h2 class="text-3xl text-center">Our Reviews</h2>
+            
+            <div id="default-carousel" class="relative w-full" data-carousel="static">
+                <!-- Carousel wrapper -->
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <!-- Item 1 -->
+                    <div class="hidden duration-700 ease-in-out pt-10" data-carousel-item>
+                        <h3 class="text-4xl text-center">Lorem ipsum odor amet, consectetuer adipiscing elit. Dui viverra posuere purus aenean quisque, blandit malesuada. Enim erat non lacus urna quisque? Mauris ad facilisis; interdum fusce mollis amet tristique. Non per dui lobortis lobortis pharetra, metus a natoque phasellus. Dictum nam eget ac posuere ac, maximus fames</h3>
+                        <h2 class="text-2xl text-center mt-10">~Pak Tatang</h2>
+                    </div>
+                    <!-- Item 2 -->
+                    <div class="hidden duration-700 ease-in-out pt-10" data-carousel-item>
+                        <h3 class="text-4xl text-center">Lorem ipsum odor amet, consectetuer adipiscing elit. Dui viverra posuere purus aenean quisque, blandit malesuada. Enim erat non lacus urna quisque? Mauris ad facilisis; interdum fusce mollis amet tristique. Non per dui lobortis lobortis pharetra, metus a natoque phasellus. Dictum nam eget ac posuere ac, maximus fames</h3>
+                        <h2 class="text-2xl text-center mt-10">~Bu Ijah</h2>
+                    </div>
+                    <!-- Item 3 -->
+                    <div class="hidden duration-700 ease-in-out pt-10" data-carousel-item>
+                        <h3 class="text-4xl text-center">Lorem ipsum odor amet, consectetuer adipiscing elit. Dui viverra posuere purus aenean quisque, blandit malesuada. Enim erat non lacus urna quisque? Mauris ad facilisis; interdum fusce mollis amet tristique. Non per dui lobortis lobortis pharetra, metus a natoque phasellus. Dictum nam eget ac posuere ac, maximus fames</h3>
+                        <h2 class="text-2xl text-center mt-10">~Bang Doel</h2>
+                    </div>
+
+                </div>
+                <!-- Slider indicators -->
+                <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                </div>
+                <!-- Slider controls -->
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
+
+            </div>
+
+            <div id="contactus" class="py-52">
+                <h2 class="text-3xl">Contact Us</h2>
+                <p>+62 812 123 4567</p>
+                <p>Jakarta, Indonesia</p>
+            </div>
+
         </div>
-        
-        <br>
-        <br>
-        <br>
+
+
         <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 
     </body>
